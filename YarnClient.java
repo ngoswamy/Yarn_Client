2e91e21180432ac756aa7bf0ac4ee228393b7633
ngoswamy <ngoswamy@hotmail.com>
2013-02-24 08:23:50 +0530

    Update YarnClient.java

--- YarnClient.java
+++ YarnClient.java
@@ -1,34 +1,18 @@
-import java.util.Map;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 import java.net.URI;
 import java.net.InetSocketAddress;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.FileStatus;
-import org.apache.hadoop.yarn.api.ApplicationConstants;
-import org.apache.hadoop.yarn.api.ClientRMProtocol;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.yarn.api.*;
 import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.hadoop.net.NetUtils;
 import org.apache.hadoop.yarn.security.client.ClientRMSecurityInfo;
-import org.apache.hadoop.yarn.api.protocolrecords.GetNewApplicationRequest;
-import org.apache.hadoop.yarn.api.protocolrecords.GetNewApplicationResponse;
-import org.apache.hadoop.yarn.api.protocolrecords.SubmitApplicationRequest;
-import org.apache.hadoop.yarn.api.protocolrecords.GetApplicationReportRequest;
-import org.apache.hadoop.yarn.api.protocolrecords.GetApplicationReportResponse;
+import org.apache.hadoop.yarn.api.protocolrecords.*;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.security.SecurityInfo;
 import org.apache.hadoop.yarn.ipc.YarnRPC;
 import org.apache.hadoop.yarn.util.Records;
 
-import org.apache.hadoop.yarn.api.records.ApplicationSubmissionContext;
-import org.apache.hadoop.yarn.api.records.ContainerLaunchContext;
-import org.apache.hadoop.yarn.api.records.Resource;
-import org.apache.hadoop.yarn.api.records.LocalResource;
-import org.apache.hadoop.yarn.api.records.LocalResourceType;
-import org.apache.hadoop.yarn.api.records.LocalResourceVisibility;
-import org.apache.hadoop.yarn.api.records.ApplicationReport;
+import org.apache.hadoop.yarn.api.records.*;
 import org.apache.hadoop.yarn.util.ConverterUtils;
 
 public class YarnClient {
@@ -42,16 +26,12 @@ public class YarnClient {
             YarnConfiguration.DEFAULT_RM_ADDRESS));             
     System.out.println("Connecting to ResourceManager at " + rmAddress);
     Configuration appsManagerServerConf = new Configuration(conf);
-/*    appsManagerServerConf.setClass(
-        YarnConfiguration.YARN_SECURITY_INFO,
-        ClientRMSecurityInfo.class, SecurityInfo.class); 
-*/
+
 try{
     YarnRPC rpc = YarnRPC.create(appsManagerServerConf);
     applicationsManager = ((ClientRMProtocol) rpc.getProxy(
         ClientRMProtocol.class, rmAddress, appsManagerServerConf));   
  
- //Next step
     GetNewApplicationRequest request = 
         Records.newRecord(GetNewApplicationRequest.class);              
     GetNewApplicationResponse response = 
@@ -71,29 +51,17 @@ try{
     // Define the local resources required 
     Map<String, LocalResource> localResources = 
         new HashMap<String, LocalResource>();
-    // Lets assume the jar we need for our ApplicationMaster is available in 
-    // HDFS at a certain known path to us and we want to make it available to
-    // the ApplicationMaster in the launched container 
-    Path jarPath= new Path("hdfs://master:9000/user/neeraj/tmp.jar"); // <- known path to jar file  
-    //FileStatus jarStatus = (new org.apache.hadoop.fs.LocalFileSystem()).getFileStatus(jarPath);
+   Path jarPath= new Path("hdfs://master:9000/user/neeraj/tmp.jar"); // <- known path to jar file  
+ 
     Configuration nconf = new Configuration();
     FileSystem fs = FileSystem.get(nconf);
     FileStatus jarStatus = fs.getFileStatus(jarPath);
     LocalResource amJarRsrc = Records.newRecord(LocalResource.class);
-    // Set the type of resource - file or archive
-    // archives are untarred at the destination by the framework
-    amJarRsrc.setType(LocalResourceType.FILE);
-    // Set visibility of the resource 
-    // Setting to most private option i.e. this file will only 
-    // be visible to this instance of the running application
-    amJarRsrc.setVisibility(LocalResourceVisibility.APPLICATION);          
-    // Set the location of resource to be copied over into the 
-    // working directory
+   
+   amJarRsrc.setType(LocalResourceType.FILE);
+   amJarRsrc.setVisibility(LocalResourceVisibility.APPLICATION);          
     amJarRsrc.setResource(ConverterUtils.getYarnUrlFromPath(jarPath)); 
-    // Set timestamp and length of file so that the framework 
-    // can do basic sanity checks for the local resource 
-    // after it has been copied over to ensure it is the same 
-    // resource the client intended to use with the application
+ 
     amJarRsrc.setTimestamp(jarStatus.getModificationTime());
     amJarRsrc.setSize(jarStatus.getLen());
     // The framework will create a symlink called AppMaster.jar in the 
@@ -106,12 +74,6 @@ try{
 
     // Set up the environment needed for the launch context
     Map<String, String> env = new HashMap<String, String>();    
-    // For example, we could setup the classpath needed.
-    // Assuming our classes or jars are available as local resources in the
-    // working directory from which the command will be run, we need to append
-    // "." to the path. 
-    // By default, all the hadoop specific classpaths will already be available 
-    // in $CLASSPATH, so we should be careful not to overwrite it.   
     String classPathEnv = "$CLASSPATH:./*:";    
     env.put("CLASSPATH", classPathEnv);
     amContainer.setEnvironment(env);
@@ -120,19 +82,9 @@ try{
     //    " arg1 arg2 arg3" + 
     List<String> commands = new ArrayList<String>();
     commands.add(command);
-    // add additional commands if needed                
-
-    // Set the command array into the container spec
     amContainer.setCommands(commands);
     
-    // Define the resource requirements for the container
-    // For now, YARN only supports memory so we set the memory 
-    // requirements. 
-    // If the process takes more than its allocated memory, it will 
-    // be killed by the framework. 
-    // Memory being requested for should be less than max capability 
-    // of the cluster and all asks should be a multiple of the min capability. 
-    Resource capability = Records.newRecord(Resource.class);
+   Resource capability = Records.newRecord(Resource.class);
     capability.setMemory(1);
     amContainer.setResource(capability);
     
@@ -144,10 +96,7 @@ try{
         Records.newRecord(SubmitApplicationRequest.class);
     appRequest.setApplicationSubmissionContext(appContext);
 
-    // Submit the application to the ApplicationsManager
-    // Ignore the response as either a valid response object is returned on 
-    // success or an exception thrown to denote the failure
-    applicationsManager.submitApplication(appRequest);
+     applicationsManager.submitApplication(appRequest);
 
 GetApplicationReportRequest reportRequest = 
           Records.newRecord(GetApplicationReportRequest.class);
@@ -159,6 +108,5 @@ GetApplicationReportRequest reportRequest =
 } catch(Exception ioe)
 { ioe.printStackTrace();
 }
-//-------------------
  }
 }
